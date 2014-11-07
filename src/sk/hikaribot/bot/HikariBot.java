4e524a0294795c893c37dd0070e71cb195f4c0c4
Shizuka Kamishima <shizuka@users.noreply.github.com>
2014-11-07 00:54:09 -0600

    successful connection and nothing else

--- src/sk/hikaribot/bot/HikariBot.java
+++ src/sk/hikaribot/bot/HikariBot.java
@@ -6,9 +6,20 @@
 
 package sk.hikaribot.bot;
 
+import java.util.Properties;
+import org.jibble.pircbot.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Class description
  */
-public class HikariBot {
+public class HikariBot extends PircBot {
+  
+  private static final Logger log = LogManager.getLogger("Bot");
+  
+  public HikariBot(Properties config) {
+    this.setName(config.getProperty("nick"));
+  }
 
 }
