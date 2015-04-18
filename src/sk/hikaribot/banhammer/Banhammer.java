eba674ff5759844d9fd1eace273c40b651b6a868
Shizuka Kamishima <shizuka@users.noreply.github.com>
2015-04-18 18:04:53 -0500

    initial frameworking, nothing wired up yet

--- src/sk/hikaribot/banhammer/Banhammer.java
+++ src/sk/hikaribot/banhammer/Banhammer.java
@@ -31,13 +31,67 @@
  */
 package sk.hikaribot.banhammer;
 
+import java.util.HashMap;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import sk.hikaribot.banhammer.api.*;
 import sk.hikaribot.bot.HikariBot;
 
 /**
- * Class description
+ * Ban management component. Allows us to keep a limit of concurrent bans set
+ * in IRC. Dispatches IRC events to BanChannel worker objects.
+ *
+ * Channels have a hard limit of bans that can be applied. Banhammer maintains
+ * an "active" ban list that mirrors the actual +b in channel. If the length of
+ * that list exceeds loThreshold, then new +b modes will cause the oldest active
+ * +b to be marked "inactive", set -b, so the list doesn't grow. The list of
+ * inactive bans, then, is scanned on user JOINs to see if the new user matches
+ * a banmask in the list. If they do, the ban is marked "active" again, set +b,
+ * and the user kicked (with appropriate message). If this would exceed the low
+ * threshold, again the oldest active ban is rotated out.
+ *
+ * If a ban is unset by operators, it is likewise marked "unset" in our list,
+ * but the record remains for logging purposes (we can see how many times a ban
+ * was set, unset, set again, etc). This leaves channel operators in full
+ * control over the active bans. MODE -b always comes through even if the mask
+ * isn't currently set, which Banhammer can pick up and check against its lists.
+ * Inactive bans can be easily unset in this way, or else by command.
+ *
+ * BanChannel worker objects are responsible for maintaining the lists of active
+ * and inactive bans, synchronizing them with the channel, and checking incoming
+ * JOINs against the inactive list. Banhammer dispatches JOIN, +b, and -b to the
+ * relevant BanChannel object. BanChannels will also construct a BanlistResponse
+ * object to be subscribed to HikariBot's ServerResponse, to scrape the current
+ * MODEs +b in their channel. They will then perform queries via BanDatabase to
+ * update the truly active bans. This only needs to happen on construction, as
+ * changes to the +b will be picked up as +b/-b thereafter and invoke logic.
+ *
+ * FUTURE ENHANCEMENTS: Timebans, channel synchronization
+ * Timebans would be implemented via a cron-like Observer to ServerResponse, or
+ * to a dedicated Cron component in HikariBot, to wake up frequently. Bans so
+ * marked would store an expiration timestamp in the database. When the time
+ * passes, the ban is simply unset.
+ * Channel synchronization would necessitate an object like BanChannel, but
+ * without any of the direct list management. They would forward their joins,
+ * bans, unbans, etc, to their parent BanChannel. The usual logic would apply,
+ * with the parent updating records, then passing any mode-setting or kicking
+ * commands back to its synchronized channels.
+ *
+ * General Architecture:
+ * Banhammer
+ * |
+ * |--BanChannel - object managing one channel
+ * |--BanChannel - receives onBan(), onUnban(), and onJoin() from HikariBot
+ * |--BanChannel - can also call out to ServerResponse for BanlistResponse
+ * | |
+ * |-+--BanDatabase - encapsulates BanChannel requests to SQLite queries
+ * . | |--SQLite database file
+ * . | |
+ * . | |--BanEntry(s) - List from a query
+ * . | |--BanNote(s) - for each ban
+ * . |
+ * . |--BanlistResponse - returns to HikariBot to gather RPL_BANLIST responses
+ * . |--ScrapedBan - trimmed BanEntry for the available info from banlisting
  *
  * @author Shizuka Kamishima
  */
@@ -46,40 +100,56 @@ public class Banhammer {
   private static final Logger log = LogManager.getLogger("BH");
   private final HikariBot bot;
   private final BanDatabase db;
+  private final HashMap<String, BanChannel> channels;
 
   public Banhammer(HikariBot bot) {
     this.bot = bot;
     this.db = new BanDatabase(bot);
-    //perform database sanity checks
-    //load default loThreshold hiThreshold kickMessage
+    this.channels = new HashMap();
   }
 
-  public void initChannel(String channel) {
-    //call initChannel with default values
+  public void addChannel(String channel) {
+    if (this.channels.containsKey(channel)) {
+      //re-init the worker?
+      return;
+    }
+    BanChannel bc = new BanChannel(this, channel);
+    this.channels.put(channel, bc);
+    //BanChannel will establish itself in the database and start scraping
   }
 
-  public void initChannel(String channel, String loThreshold, String hiThreshold, String kickMessage) {
-    //initialize channel worker object
-  }
-
-  public void enableChannel(String channel) {
-    //turn on ban rotation and monitoring for channel
-  }
-
-  public void disableChannel(String channel) {
-    //opposite of that
+  public void removeChannel(String channel) {
+    if (!this.channels.containsKey(channel)) {
+      return;
+    }
+    //BanChannel bc = this.channels.get(channel);
+    //we may need that for any destruction logic (like final commits?)
+    this.channels.remove(channel);
   }
 
   public void onJoin(String channel, String usermask) {
-    //pass usermask to channel worker for checking against inactive list
+    if (!this.channels.containsKey(channel)) {
+      return;
+    }
+    this.channels.get(channel).onJoin(usermask);
   }
-  
+
   public void onBan(String channel, String banmask, String userWhoSet) {
-    log.debug("MODE " + channel + " +b " + banmask + " BY " + userWhoSet);
+    if (!this.channels.containsKey(channel)) {
+      return;
+    }
+    this.channels.get(channel).onBan(banmask, userWhoSet);
   }
 
   public void onUnban(String channel, String banmask, String userWhoSet) {
-    log.debug("MODE " + channel + " -b " + banmask + " BY " + userWhoSet);
+    if (!this.channels.containsKey(channel)) {
+      return;
+    }
+    this.channels.get(channel).onUnban(banmask, userWhoSet);
+  }
+
+  public BanDatabase getDatabase() {
+    return this.db;
   }
 
 }
---
+++ src/sk/hikaribot/banhammer/api/BanChannel.java
@@ -0,0 +1,202 @@
+/*
+ * hikaribot - BanChannel
+ * Shizuka Kamishima - 2015-04-15
+ * 
+ * Copyright (c) 2015, Shizuka Kamishima
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package sk.hikaribot.banhammer.api;
+
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import sk.hikaribot.banhammer.Banhammer;
+
+/**
+ * Handles Banhammer operations for a single channel.
+ *
+ * @author Shizuka Kamishima
+ */
+public class BanChannel implements Observer {
+
+  private static final Logger log = LogManager.getLogger("BHC");
+  private final Banhammer bh;
+  private final BanDatabase db;
+  private final String channel;
+
+  /**
+   * Number of active bans above which we will rotate out oldest ban on a new +b
+   */
+  private int loThreshold;
+
+  /**
+   * Number of active bans above which we will rotate out oldest ban until equal
+   */
+  private int hiThreshold;
+
+  /**
+   * Message to be sent to user upon being kickbanned for Inactive->Active ban
+   */
+  private String kickMessage;
+
+  //Table names
+  private final String tbBans;
+  private final String tbNotes;
+  private final String tbOptions;
+  private final String tbTemp;
+
+  /**
+   * Initialize and perform sanity checks.
+   *
+   * @param bh our parent Banhammer
+   * @param channel the channel we operate in
+   */
+  public BanChannel(Banhammer bh, String channel) {
+
+    //wake up
+    this.bh = bh;
+    this.db = bh.getDatabase();
+    this.channel = channel;
+
+    //define our table names
+    String prefix = "bh_" + channel;
+    this.tbBans = prefix + "_bans";
+    this.tbNotes = prefix + "_notes";
+    this.tbOptions = prefix + "_options";
+    this.tbTemp = prefix + "_temp";
+
+    //check if our tables exist
+    //create if not
+    
+    //if our options table exists, load thresholds and message
+    //else copy options from bh_global_options
+    //BanDatabase will have already checked that
+    
+    log.info("Initialized in " + channel);
+  }
+
+  /**
+   * Input options to this channel. For use when just starting out.
+   *
+   * @param loThreshold above this we rotate old bans to make room for +b
+   * @param hiThreshold above this we inactive bans until we meet it
+   * @param kickMessage message for user upon being kickbanned on inactive
+   */
+  public void setOptions(int loThreshold, int hiThreshold, String kickMessage) {
+    //set options in tbOptions
+    //load to our vars
+  }
+
+  /*
+   * ON INIT
+   * scrape banlist from channel
+   * merge scraped list with database
+   * -any mask found in db becomes Active
+   * -any mask not found in db gets ADDed, and becomes Active
+   * merge database active list back with banlist
+   * -any Active mask not found in scraped list becomes Unset
+   * -any Permanent mask not found in scraped list is put on SET list
+   * check timebans
+   * -all timebans whose expiration passed, put on UNSET list and become Unset
+   * [PRINT STATUS MESSAGE: Banlist scraped, found X bans]
+   * check threshold
+   * -if Permanent+Active+Timeban exceeds hiThreshold
+   * -mark Inactive the oldest Active bans until hiThreshold is met
+   * -put those on UNSET list
+   * apply modes
+   * -foreach SET list, ban
+   * -foreach UNSET list, unban
+   * [PRINT STATUS MESSAGE: X/threshold active bans, tracking Y inactive bans
+   *
+   */
+  
+  /**
+   * Create a BanlistResponse and send request to HikariBot for server.
+   */
+  public void requestBanlist() {
+    //create BanlistResponse
+    //subscribe to it
+    //subscribe it to ServerResponse
+  }
+  
+  //BanlistResponse has called us, pass it along and destroy the object
+  @Override
+  public void update(Observable o, Object arg) {
+    //o is the BanlistResponse
+    //arg is a List<BanEntry> of bans
+    
+    //unsubscribe o from ServerResponse
+  }
+  
+  /**
+   * Handle the returning List of bans from BanlistResponse.
+   * @param scrapedBans 
+   */
+  public void scrapeBanlist(List<ScrapedBan> scrapedBans) {
+    /*
+     * get list of all bans from BanDatabase
+     * merge channel active list with database
+     * --if mask exists in db, mark it Active
+     * --if mask is not in db, insert it Active
+     * merge database active list with channel list
+     * --if mask is not in active list, mark it Unset
+     * --if mask is permanent and not in active list, SET it
+     */
+  }
+  
+  /**
+   * Incoming JOIN, check against the Inactive ban list.
+   * @param usermask 
+   */
+  public void onJoin(String usermask) {
+    //get inactive list from database
+    //regex against the list for this usermask
+    //if it matches, call function to transfer inactive->active
+  }
+
+  public void onBan(String banmask, String userWhoSet) {
+    log.debug("MODE " + channel + " +b " + banmask + " BY " + userWhoSet);
+  }
+
+  public void onUnban(String banmask, String userWhoSet) {
+    log.debug("MODE " + channel + " -b " + banmask + " BY " + userWhoSet);
+  }
+  
+  /*
+   * TODO: configuration and query ops
+   * like search ban by id, mask, nick
+   * dump notes to PM
+   * SmartUnban? (given nick, try to whois it, match usermask against DB)
+   * SmartBan? (whowas it, etc)
+   * logging commands: loglast, loglastN, logban by id
+   * setting commands: unban by id, unbanLast, force active/inactive?
+   * permanent bans config
+   */
+
+}
